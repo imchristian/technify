bfb522e8afe2c5f0a60e30c43bf82491030f3bbe
imchristian_g <cristian.gibert@mercadolibre.com>
2015-11-24 21:20:10 -0300

    ajax call to send email

--- enviado.php
+++ enviado.php
@@ -13,17 +13,10 @@ if($_POST["nombre"] && $_POST["email"] && $_POST["consulta"] != ""){
 	$mensaje .= "\n";
 	$mensaje .= "Consulta: " . utf8_decode($_POST["consulta"]) ."\n";
     $emailheader = "From: Blow Consulta <info@it360.com.ar>\r\n";
-mail($destino, $asunto, $mensaje, $emailheader) or die ("Lo sentimos, tu solicitud no ha sido enviada.<br/>Intentelo de nuevo.");
-echo utf8_decode(utf8_encode('Tu consulta ha sido enviada correctamente.'));
+	mail($destino, $asunto, $mensaje, $emailheader) or die ("false");
+	echo "true";
 	} else {
-    if($_POST["nombre"] == ""){
-    echo utf8_encode ('Por favor, indica tu nombre.');
-    exit; }
-    if($_POST["email"] == ""){
-    echo utf8_encode ('Por favor, indica un email de contacto.');
-    exit; }
-    if($_POST["consulta"] == ""){
-    echo utf8_encode ('Por favor, indica tu consulta.');
-    exit; }
+    	echo "false;"
+     }
 }
 ?>
